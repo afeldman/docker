27544439dbc97a8341810831fe387ecc906091b2
anton feldmann <anton.feldmann@gmail.com>
2018-01-26 12:22:18 +0900

    new base

--- base/cuda/build_nvidia.sh
+++ base/cuda/build_nvidia.sh
@@ -1,12 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
-chmod +x cuda_*_linux.run
+#export TERM='/bin/sh'
 
-cuda_*_linux.run --tar mxvf
-./run_files/cuda-linux64-rel-*.run -noprompt
+#echo $(ls /usr/lib/perl5/core_perl)
 
-rm -rf cuda_*_linux.run \
-   /run_files \
-   InstallUtils.pm \
-   cuda-installer.pl \
-   uninstall_cuda.pl
+chmod +x /cuda/cuda_*_linux.run
+
+./cuda_*_linux.run --toolkit --driver --samples --override --silent
+
+#/cuda/*.run -s -N --no-kernel-module 
+
+#./cuda_*_linux.run --tar mxvf
+#copy InstallUtils.pm to /usr/lib/x86_64-linux-gnu/perl-base 
+
+#./run_files/cuda-linux64-rel-*.run -noprompt
+
+#rm -rf cuda_*_linux.run \
+#   /run_files \
+#   InstallUtils.pm \
+#   cuda-installer.pl \
+#   uninstall_cuda.pl
